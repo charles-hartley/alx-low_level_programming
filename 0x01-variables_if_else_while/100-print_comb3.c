c9e8d7014ac3a8a71818ab8ea1e14e80ec7297e7
charles-hartley <charles-hartley>
2023-09-10 03:22:25 -0700

    we ate

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    for (int i = 0; i <= 9; i++)
+    {
+        for (int j = i + 1; j <= 9; j++)
+        {
+            putchar('0' + i);
+            putchar('0' + j);
+            if (i < 9 || j < 8)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
