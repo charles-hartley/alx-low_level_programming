e76dbab80346543b9cdfbde74aa31280e76c7e8a
charles-hartley <charles-hartley>
2023-09-10 04:04:19 -0700

    brain

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
+
 int main(void)
 {
-    for (int i = 0; i <= 9; i++)
-    {
-        for (int j = i + 1; j <= 9; j++)
-        {
-            putchar('0' + i);
-            putchar('0' + j);
-            if (i < 9 || j < 8)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i < 9 || j < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
