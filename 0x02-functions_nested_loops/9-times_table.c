7abee0c7d0154c6aa43c800c43fd84d9998b62c2
charles-hartley <charles-hartley>
2023-09-13 16:09:04 -0700

    learn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,8 +3,7 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-
-void times_tables(void)
+void times_table(void)
 {
 	int i, j, k;
 
@@ -15,7 +14,7 @@ void times_tables(void)
 			k = j * i;
 			if (j == 0)
 			{
-				_putchar(k + '0')
+				_putchar(k + '0');
 			}
 
 			if (k < 10 && j != 0)
@@ -24,15 +23,15 @@ void times_tables(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
-			else if
-				(k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
 		}
 		_putchar('\n');
 	}
+}
+
